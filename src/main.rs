bdb8d645849cbcae6879b0eba555d6c6c2ca88bf
Max Dobeck <msdobeck@gmail.com>
2019-06-09 21:33:49 -0700

    extended example working. functions added and input is returned as a String.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use std::io;
 use std::io::Write;
 
 fn main() {
+    let input = get_input();
+    println!("{}", input);
+}
+
+fn get_input() -> String{
     print!("Enter your text: ");
     io::stdout().flush().unwrap();
     let mut val = String::new();
 
     io::stdin().read_line(&mut val)
         .expect("Error getting input");
-
-    println!("{}", val)
+    
+    return val;
 }
 
