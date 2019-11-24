306a9345cd4bbaf4fd726b257b6d8d889c07ee3e
Max Dobeck <msdobeck@gmail.com>
2019-11-23 18:00:13 -0800

    Added full echo functionality.

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use std::io::Write;
 
 fn main() {
     let input = get_input();
-    println!("{}", input);
+    let s: &str = &input[..];
+    echo(s);
 }
 
 fn get_input() -> String {
-    // print!("Enter your text: ");
-    // Alternative to printing literal text.  Use an IMMUTABLE variable instead!
-    let prompt = "Enter your text:";
+    let prompt: String;
+    prompt = "Enter your text:".to_string();
     print!("{}", prompt);
 
     io::stdout().flush().unwrap();
@@ -20,4 +20,11 @@ fn get_input() -> String {
         .expect("Error getting input");
 
     return val;
+}
+
+fn echo(input: &str) {
+    for i in (1..input.len()).rev() {
+        print!("{}\n", &input[..i]);
+    }
+
 }
